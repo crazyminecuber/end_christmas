dfbd482628874a0cadc4bd931b7fa8a513ae44c0
evewa104 <evewa104@tlhw-4-4.ad.liu.se>
2020-11-15 12:10:32 +0100

    Skelett till Tower.cc

---
+++ Tower.cc
@@ -0,0 +1,47 @@
+#inlcude "Tower.h"
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Funktioner som tillhör klassen Tower
+void Tower::collision(Entity* object)
+{
+
+};
+
+void Tower::shoot()
+{
+  if (!shootable_enemies.empty())
+  {
+    std::string the_enemy = shootable_enemies[1];
+
+  }
+};
+
+void Tower::make_projectile(Vector2f velocity)
+{
+  Projectiles* 
+};
+
+//Funktioner som tillhör klassen Tower_basic
+void Tower_basic::shoot()
+{
+
+};
+
+Entity Tower_basic::select_target()
+{
+
+};
+
+vector2<float> Tower_basic::aim()
+{
+
+};
+
+//Funktioner som tillhör klassen Tower_ring
+void Tower_ring::shoot()
+{
+
+};
