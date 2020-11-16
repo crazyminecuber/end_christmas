15b6ab952a5510da97cc34937931d8676912c8fa
mikjo695 <mikjo695@tlhw-5-3.ad.liu.se>
2020-11-16 08:20:03 +0100

    Enemy ärver nu från entity

--- Enemy.h
+++ Enemy.h
@@ -1,9 +1,18 @@
 #include <vector>
+#include "Entity.h"
 
-class Enemy
+class Enemy : public Entity
 {
     public:
-        Enemy();
+        Enemy(std::string texture_file, sf::Vector2f position,
+              sf::Vector2f siz, float hit_rad,
+              sf::Vector2f dir, float mov_spd,
+              int arg_life):
+              Entity(std::string texture_file,
+              sf::Vector2f position, sf::Vector2f siz,
+              float hit_rad, sf::Vector2f dir, float mov_spd),
+              life{arg_life}{}
+
         ~Enemy();
         static std::vector<Enemy*> enemies;
     private:
