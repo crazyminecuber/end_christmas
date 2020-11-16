d61345c735e2dd916b89998177778a6eb6c22aa2
hilka588 <hilka588@tlhw-5-4.ad.liu.se>
2020-11-16 09:58:38 +0100

    Continue with collision next time

--- Projectile.cc
+++ Projectile.cc
@@ -6,11 +6,18 @@ using namespace std;
 
 //Funktioner som tillhör Projectile
 Projectile::Projectile()
-{}
+{
+  for(int a = 0; frame_to_die > a; a++)
+  {
+    //Uppdate position?
+  }
+  //delete projectile if beyond frame_to_die
+  delete Projectile;
+}
 
 void Projectile::update_position()
   {
-
+    Projectile.position += Projectile.mov_spd * Projectile.dir;
   }
 
 //Funktioner som tillhör Projectile_basic
@@ -19,8 +26,19 @@ Projectile::Projectile_basic()
 
 void Projectile::collision(Entity* object)
   {
+    vector<Object> objs = projectiles;
+    objs.insert = enemies;
 
-  }
+    for (auto curr{objs.begin()}; curr != objs.end(); ++curr)
+      {
+        for (auto next{curr+1}; next != objs.end(); ++next)
+        {
+          if (collide(*curr, *next))
+          {
+            // två? objekt har kolliderat
+          }
+        }
+      }
 
 //Funktion som tillhör Projectile_pierce
 Projectile::Projectile_pierce
@@ -37,13 +55,13 @@ Projectile::Projectile_bomb
 
 void Projectile::collision(Entity* object)
   {
-
+    
   }
 
 //Funktion som tillhör Projectile_bomb_blast
 Projectile::Projectile_bomb_blast
 {}
-  
+
 void Projectile::collision(Entity* object)
   {
 
