123989e7038c92d46f5a6ce42e49491bdeed8e94
hilka588 <hilka588@tlhw-5-4.ad.liu.se>
2020-11-17 11:54:50 +0100

    Written collision, next time destructor

--- Projectile.cc
+++ Projectile.cc
@@ -9,16 +9,16 @@ Projectile::Projectile()
 {
   for(int a = 0; frame_to_die > a; a++)
   {
-    //Uppdate position?
+    Projectile*->update_position();
   }
   //delete projectile if beyond frame_to_die
-  delete Projectile;
-}
+  delete Projectile*;
+};
 
 void Projectile::update_position()
   {
     Projectile.position += Projectile.mov_spd * Projectile.dir;
-  }
+  };
 
 //Funktioner som tillhör Projectile_basic
 Projectile::Projectile_basic()
@@ -26,43 +26,116 @@ Projectile::Projectile_basic()
 
 void Projectile::collision(Entity* object)
   {
-    vector<Object> objs = projectiles;
-    objs.insert = enemies;
-
-    for (auto curr{objs.begin()}; curr != objs.end(); ++curr)
+    vector<Enemy*> enemies;
+    vector<Projectile*> projectiles;
+    for (auto curr_proj{projectiles.begin()}; curr_proj != projectiles.end(); ++curr_proj)
       {
-        for (auto next{curr+1}; next != objs.end(); ++next)
+        for (auto curr_enemy{enemies.begin()}; curr_enemy != enemies.end(); ++curr_enemy)
         {
-          if (collide(*curr, *next))
+          if (*curr_proj.hit_rad == *curr_enemy.hit_rad)
           {
-            // två? objekt har kolliderat
+            //delete in Enemy that will delete Enemy
+            delete *curr_proj;
           }
         }
       }
-
+  };
 //Funktion som tillhör Projectile_pierce
 Projectile::Projectile_pierce
-{}
+{
+  for(int a = 0; frame_to_die > a; a++)
+  {
+    Projectile*->update_position();
+  }
+  //delete projectile if beyond frame_to_die
+  delete Projectile*;
+}
 
 void Projectile::collision(Entity* object)
   {
-
-  }
+  vector<Enemy*> enemies;
+  vector<Projectile*> projectiles;
+  int nr_enemies_killed = 0;
+  for (auto curr_proj{projectiles.begin()}; curr_proj != projectiles.end(); ++curr_proj)
+    {
+      for (auto curr_enemy{enemies.begin()}; curr_enemy != enemies.end(); ++curr_enemy)
+      {
+        if (*curr_proj.hit_rad == *curr_enemy.hit_rad)
+        {
+          //delete in Enemy that will delete Enemy
+          if(nr_enemies_killed <= nr_pierce)
+          {
+            //uppdate next enemy
+          }
+          else
+          {
+            delete *curr_proj;
+          }
+        }
+      }
+    }
+  };
 
 //Funktion som tillhör Projectile_bomb
 Projectile::Projectile_bomb
-{}
+{
+  for(int a = 0; frame_to_die > a; a++)
+  {
+    Projectile*->update_position();
+  }
+  //delete projectile if beyond frame_to_die
+  delete Projectile*;
+}
 
 void Projectile::collision(Entity* object)
   {
+  vector<Enemy*> enemies;
+  vector<Projectile*> projectiles;
+  for (auto curr_proj{projectiles.begin()}; curr_proj != projectiles.end(); ++curr_proj)
+    {
+      for (auto curr_enemy{enemies.begin()}; curr_enemy != enemies.end(); ++curr_enemy)
+      {
+        if (*curr_proj.hit_rad == *curr_enemy.hit_rad)
+        {
+          //delete in Enemy that will delete Enemy
+          projectiles.add<new Projectile_bomb_blast*>;
+          delete *curr_proj;
+        }
+      }
+    }
 
-  }
+  };
 
 //Funktion som tillhör Projectile_bomb_blast
 Projectile::Projectile_bomb_blast
-{}
+{
+  for(int a = 0; frame_to_die > a; a++)
+  {
+    Projectile*->update_radius();
+  }
+  //delete projectile if beyond frame_to_die
+  delete Projectile*;
+}
+
+void Projectile::update_radius(Entity* object)
+{
+  Projectile.hit_rad += blast_rad;
+};
 
 void Projectile::collision(Entity* object)
-  {
+{
+  vector<Enemy*> enemies;
+  vector<Projectile*> projectiles;
+  for (auto curr_proj{projectiles.begin()}; curr_proj != projectiles.end(); ++curr_proj)
+    {
+      for (auto curr_enemy{enemies.begin()}; curr_enemy != enemies.end(); ++curr_enemy)
+      {
+        if (*curr_proj.hit_rad == *curr_enemy.hit_rad)
+        {
+          //delete in Enemy that will delete Enemy
+          delete *curr_proj;
+        }
+      }
+    }
 
   }
