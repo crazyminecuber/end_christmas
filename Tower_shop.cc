893cba9aa02ae579ed0b77a06d87ad1b5932fa31
Oskar Philipsson <oskph717@student.liu.se>
2020-11-21 22:58:40 +0100

    Tower buttons now placed in a dynamic grid

--- Tower_shop.cc
+++ Tower_shop.cc
@@ -4,22 +4,72 @@ using namespace std;
 
 Tower_shop::Tower_shop(std::vector<Tower *> pt, Wallet w, sf::Vector2f pos,
         sf::Vector2f siz, sf::Vector2f btn_size)
-    : RectangleShape(siz), passive_towers{pt}, wallet{w}, button_size{btn_size} // Lagra position och size i sfml:objectet.
+    : RectangleShape(siz), passive_towers{pt}, wallet{w}, button_size{btn_size},
+    heading{make_text()}// Lagra position och size i sfml:objectet.
 {
     setPosition(pos);
     chosen_tower = nullptr;
+    // Set position of text. Gör smartare!
+    heading.setPosition(Vector2f(150,150));
     generate_shop_grid();
 }
 
-void Tower_shop::generate_shop_grid() // Genera knappar med textur genom att kalla på tower_button många gånger.
+void Tower_shop::generate_shop_grid(int nr_columns, IntRect area) // Genera knappar med textur genom att kalla på tower_button många gånger.
 {
     // Just to test, will add more advanced stuff later.
-    Tower_button(passive_towers.front(), this, button_size);
+    // What determins all the sizes? The shop will have a given with. When it
+    // has taken all the space for the text and potentiall other thing at the
+    // top, a rectangle will be created that tower_buttons are allowed to be
+    // placed in. Then we assume that we are given (or computed elsewere), the
+    // number of collumns, the button size. Then the spacing can be calculated.
+
+    // Idea! Given the width of the rectangle and the width of the button,
+    // calculate the spacing between the center of each button and another
+    // center of a button or the edge of the rectangle
+
+    // Make a button for every tower
+
+    int spacing = (area.width - nr_columns * button_size.x) / (nr_columns + 1);
+    int current_column{1};
+    int current_row{1};
+    Vector2f btn_pos{};
+    for (auto tow_it = passive_towers.begin();
+         tow_it != passive_towers.end();
+         tow_it++)
+    {
+        // Lokala eller globala koordinater?
+        // Global for now
+        btn_pos.x = area.left + spacing * (current_column) + button_size.x * current_column - 1 / 2 * button_size.x;
+        btn_pos.y = area.top + spacing * (current_row) + button_size.x * current_row - 1 / 2 * button_size.y;
+        Tower_button(*tow_it, this, button_size, btn_pos);
+
+        current_column++;
+        if (current_column > nr_columns)
+        {
+            current_row++;
+            current_column = 1;
+        }
+    }
+
+    // Simple one for debugging purposes. Only crates one tower button.
+    //Tower_button(passive_towers.front(), this, button_size);
+}
+
+sf::Text Tower_shop::make_text()
+{
+    Font * font = new Font{}; // Minnesläcker!Bör nog laddas i game istället.
+    if ( !font->loadFromFile ("resources/fonts/best_font.ttf") )
+    {
+        // kunde inte ladda typsnitt
+        cout << "Unable to load font" << endl;
+    }
+    return Text{"Tower Shop", *font}; // Lite oeffektivt, men förhoppningvis så finns en flyttningkonstrutor.
 }
 
 //----------------------------------------------------------------------------
 
-Tower_button::Tower_button(Tower * tw, Tower_shop * ts, Vector2f btn_size)
+Tower_button::Tower_button(Tower * tw, Tower_shop * ts, Vector2f & btn_size,
+                            Vector2f & position)
                             : RectangleShape{btn_size}, tower{tw}, tower_shop{ts}, pricetag{make_pricetag(tw)},
                                 tower_pic{make_tower_pic(tw)}
 {
@@ -36,6 +86,8 @@ Tower_button::Tower_button(Tower * tw, Tower_shop * ts, Vector2f btn_size)
 
     //Set background.
     setFillColor(Color{0,0,100}); // hard coded color, should change
+    setOrigin(btn_size / 2.0f); // Center
+    setPosition(position);
 
     tower_shop->drawable.push_back(this);
     tower_shop->drawable.push_back(&pricetag);
--- Tower_shop.h
+++ Tower_shop.h
@@ -1,3 +1,7 @@
+//TODO add color variables for background of shop and button?
+//TODO change button size to only be the side length to guarantee it to be
+//TODO place prictag and sprite resonable
+//square?
 #ifndef TOWER_SHOP_H
 #define TOWER_SHOP_H
 
@@ -19,13 +23,18 @@ private:
     std::vector<Tower *> passive_towers;
     Wallet wallet;
     sf::Vector2f button_size;
-    void generate_shop_grid(); // Genera knappar med textur genom att kalla på tower_button många gånger.
+    sf::Text heading{};
+
+    sf::Text make_text();
+    void generate_shop_grid(int nr_columns, sf::IntRect area); // Genera knappar med textur genom att kalla på tower_button många gånger.
+
 };
 
+// Can we make it position it self relative to its center point?
 class Tower_button : public sf::RectangleShape
 {
 public:
-    Tower_button(Tower * tw, Tower_shop * ts, sf::Vector2f btn_size);
+    Tower_button(Tower * tw, Tower_shop * ts, sf::Vector2f & btn_size, sf::Vector2f & position);
 
     void on_click();
 
