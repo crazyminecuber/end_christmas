1e4066bc6cd63716d24cd2e2f4c06886c5c87ee9
hilka588 <hilka588@tlhw-4-4.ad.liu.se>
2020-11-26 17:25:32 +0100

    Made the code look nicer, made comments to some funtions and deleted some comments

--- Projectile.cc
+++ Projectile.cc
@@ -8,53 +8,58 @@
 
 using namespace std;
 
-//Tillhör klass Projectile
+//Projectile
+//Static Projectile
 vector<Projectile*> Projectile::projectiles;
 
-//Next posistion
+//Update position and return true if its still alive or false if frame>frame_to_die
 bool Projectile::update_position()
+{
+    if (Game::get_frame() < frame_to_die)
     {
-        if (Game::get_frame() < frame_to_die)
-        {
-            move(direction*movement_speed);
-            return true;
-        }
-        else
-        {
-          return false;
-        }
+        move(direction*movement_speed);
+        return true;
     }
+    else
+    {
+      return false;
+    }
+}
 
-//Kopieringskonstruktor
+//Copy-constructor, sets frame_to_die and damage
 Projectile::Projectile(Projectile const& other):Entity(other)
 {
   damage = other.damage;
   frame_to_die = other.frame_to_die;
 }
 
-//Tillhör Projectile_basic
+/*----------------------------------------------------------------------------*/
+//Projectile_basic
+//statis Projectile_basic
 entity_properties Projectile_basic::prop;
 int Projectile_basic::frames_to_live;
 int Projectile_basic::damage_init;
 
 Projectile_basic::Projectile_basic(sf::Vector2f position,sf::Vector2f direction)
   : Projectile
-    (
-      prop.texture_file,
-      position,
-      prop.size,
-      prop.hit_rad,
-      direction,
-      prop.mov_spd,
-      damage_init
-    )
+  (
+    prop.texture_file,
+    position,
+    prop.size,
+    prop.hit_rad,
+    direction,
+    prop.mov_spd,
+    damage_init
+  )
 {}
-//Kopieringskonstruktor som lägger in i lista
+
+//Copy-constructor, sets frame_to_die
 Projectile_basic::Projectile_basic(Projectile_basic const& other)
     : Projectile(other)
 {
-    frame_to_die = Game::get_frame() + frames_to_live;
+  frame_to_die = Game::get_frame() + frames_to_live;
 }
+
 //clone and add direction void clone(sf::Vector2f direction, sf::Vector2f position)
 void Projectile_basic::clone(sf::Vector2f dir, sf::Vector2f pos)
 {
@@ -63,21 +68,20 @@ void Projectile_basic::clone(sf::Vector2f dir, sf::Vector2f pos)
   p->setPosition(pos);
   projectiles.push_back(p);
 }
-//Removes and delete the porjectile when collided with an enemy
+
+// Returns true if the porjectile should be deleted when collided with an enemy
 bool Projectile_basic::collision()
     {
-      //Will be deleted on the next update_position
       return true;
-
     }
 
-
-//Tillhör Projectile_pierce
+/*----------------------------------------------------------------------------*/
+//Projectile_pierce
+//static Projectile_pierce
 entity_properties Projectile_pierce::prop;
 int Projectile_pierce::frames_to_live;
 int Projectile_pierce::damage_init;
 int Projectile_pierce::nr_pierce;
-
 
 Projectile_pierce::Projectile_pierce
 (
@@ -95,13 +99,15 @@ Projectile_pierce::Projectile_pierce
       damage_init
     )
 {}
-//Kopieringskonstruktor som lägger in i lista
+
+//Copy-constructor, sets frame_to_die
 Projectile_pierce::Projectile_pierce(Projectile_pierce const& other)
     : Projectile(other)
 {
     frame_to_die = Game::get_frame() + frames_to_live;
 }
-//clone and add direction
+
+//Clone and add to vector projectiles
 void Projectile_pierce::clone(sf::Vector2f dir, sf::Vector2f pos)
 {
   Projectile_pierce* p = new Projectile_pierce{*this};
@@ -109,29 +115,29 @@ void Projectile_pierce::clone(sf::Vector2f dir, sf::Vector2f pos)
   p->setPosition(pos);
   projectiles.push_back(p);
 }
-//Counts nr of enemies killed and remove and delete the projectile when
-// nr_pierce nr of enemies is killed
+
+//Counts nr of enemies killed and return true if the projectile should be delete
+//when nr_pierce nr of enemies is killed
 bool Projectile_pierce::collision()
 {
-    //delete in Enemy that will delete Enemy
-    if(nr_enemies_killed <= nr_pierce)
-    {
-        nr_enemies_killed+= 1;
-        return false;
-    }
-    else
-    {
-      //Will be deleted on the next update_position
-      return true;
-    }
+  if(nr_enemies_killed <= nr_pierce)
+  {
+      nr_enemies_killed+= 1;
+      return false;
+  }
+  else
+  {
+    return true;
+  }
 }
 
-
-//Funktion som tillhör Projectile_bomb
+/*----------------------------------------------------------------------------*/
+//Projectile_bomb
+//Static Projectile_bomb
 entity_properties Projectile_bomb::prop;
 int Projectile_bomb::frames_to_live;
 int Projectile_bomb::damage_init;
-
+//static Projectile_bomb_blast
 entity_properties Projectile_bomb_blast::prop;
 int Projectile_bomb_blast::frames_to_live;
 int Projectile_bomb_blast::damage_init;
@@ -152,16 +158,16 @@ Projectile_bomb::Projectile_bomb
         damage_init
       ),
       blast{}
-  {
+  {}
 
-  }
-//Kopieringskonstruktor som lägger in i lista
+//copy-constructor, copy blast and sets frame_to_die
 Projectile_bomb::Projectile_bomb(Projectile_bomb const& other)
     : Projectile(other), blast{other.blast}
 {
   frame_to_die = Game::get_frame() + frames_to_live;
 }
-//clone and add direction
+
+//clone and adds to vector projectiles
 void  Projectile_bomb::clone(sf::Vector2f dir, sf::Vector2f pos)
 {
   Projectile_bomb* p = new Projectile_bomb{*this};
@@ -170,17 +176,17 @@ void  Projectile_bomb::clone(sf::Vector2f dir, sf::Vector2f pos)
   projectiles.push_back(p);
 }
 
-//adds bomb_blast to projectiles vector and remove and delete the projectile
+//Clone object blast and return true when the projectile should be delete
+//when collided
 bool Projectile_bomb::collision()
 {
   blast.clone(sf::Vector2f(0,0), getPosition());
-  //Will be deleted on the next update_position
   return true;
 }
 
+/*----------------------------------------------------------------------------*/
 
-// Gör om bomb_blast så att den endast innehåller rad, texture, mm,
-//Funktion som tillhör Projectile_bomb_blast
+// Projectile_bomb_blast
 Projectile_bomb_blast::Projectile_bomb_blast()
     : Projectile
       (
@@ -198,13 +204,14 @@ Projectile_bomb_blast::Projectile_bomb_blast(string texture_file, sf::Vector2f p
       sf::Vector2f size, float hit_rad, sf::Vector2f dir, float mov_spd)
       : Projectile(texture_file, position, size, hit_rad, dir, mov_spd, damage_init){}
 
-//Kopieringskonstruktor som lägger in i lista
+//copy-constructor, sets frame_to_die
 Projectile_bomb_blast::Projectile_bomb_blast(Projectile_bomb_blast const& other)
     : Projectile(other)
 {
   frame_to_die = Game::get_frame() + frames_to_live;
 }
 
+//clone and adds to vector projectiles
 void Projectile_bomb_blast::clone(sf::Vector2f dir, sf::Vector2f pos)
 {
   Projectile_bomb_blast *p = new Projectile_bomb_blast{*this};
@@ -213,10 +220,8 @@ void Projectile_bomb_blast::clone(sf::Vector2f dir, sf::Vector2f pos)
   projectiles.push_back(p);
 }
 
-//remove and delete the projectile when collided with enemies
+//Returnes false, will not be deleted when collided
 bool Projectile_bomb_blast::collision()
 {
-    //delete in Enemy that will delete Enemy
-    //Will be deleted on the next update_position
     return false;
 }
--- Projectile.h
+++ Projectile.h
@@ -13,21 +13,18 @@ public:
         float hit_rad, sf::Vector2f dir, float mov_spd, int arg_damage):
         Entity(texture_file, position, size, hit_rad, dir, mov_spd),
         damage{arg_damage}{}
-
   ~Projectile()=default;
 
-  Projectile(Projectile const& other); //Kopieringskonstruktor
+  Projectile(Projectile const& other); //Copy-constructor
   virtual void clone(sf::Vector2f dir, sf::Vector2f pos)=0;
-
   bool update_position();
   virtual bool collision()=0;
+  //static
   static std::vector<Projectile*> projectiles;
- // static entity_properties prop; // ska Projectile ha Entity prop?
 
 protected:
   int damage{};
   int frame_to_die{};
-
 };
 
 class Projectile_basic : public Projectile
@@ -38,17 +35,14 @@ public:
        Projectile(texture_file, position, size, hit_rad, dir, mov_spd, arg_damage){}
   Projectile_basic(sf::Vector2f position, sf::Vector2f direction);
   ~Projectile_basic() = default;
-  Projectile_basic(Projectile_basic const& other); //Kopieringskonstruktor
+
+  Projectile_basic(Projectile_basic const& other); //Copy-constructor
   void clone(sf::Vector2f dir, sf::Vector2f pos);
   bool collision();
+  //static
   static int frames_to_live;
   static int damage_init;
   static entity_properties prop;
-protected:
-  // std::string texture_file;
-  // float hit_rad;
-  // float mov_spd;
-
 };
 
 class Projectile_pierce : public Projectile
@@ -60,19 +54,17 @@ public:
   Projectile_pierce(sf::Vector2f position,sf::Vector2f direction);
   ~Projectile_pierce() = default;
 
-  Projectile_pierce(Projectile_pierce const& other); //Kopieringskonstruktor
+  Projectile_pierce(Projectile_pierce const& other); //Copy-constructor
   void clone(sf::Vector2f dir, sf::Vector2f pos);
   bool collision();
+  //static
   static int frames_to_live;
   static int damage_init;
   static int nr_pierce;
   static entity_properties prop;
-protected:
-  // std::string texture_file;
-  // float hit_rad;
-  // float mov_spd;
-  int nr_enemies_killed;
 
+protected:
+  int nr_enemies_killed;
 };
 
 class Projectile_bomb_blast : public Projectile
@@ -84,40 +76,36 @@ public:
   ~Projectile_bomb_blast()=default;
 
   void clone(sf::Vector2f dir, sf::Vector2f pos);
-  Projectile_bomb_blast(Projectile_bomb_blast const& other); //Kopieringskonstruktor
+  Projectile_bomb_blast(Projectile_bomb_blast const& other); //Copy-constructor
   bool collision();
+  //static
   static int frames_to_live;
   static int damage_init;
   static entity_properties prop;
-
 };
 
 class Projectile_bomb : public Projectile
 {
 public:
   Projectile_bomb(std::string texture_file, sf::Vector2f position,
-        sf::Vector2f size, float hit_rad, sf::Vector2f dir, float mov_spd, int arg_damage):
-        Projectile(texture_file, position, size, hit_rad, dir, mov_spd, arg_damage),
-        blast{}
-  {
-
-  }
+                  sf::Vector2f size, float hit_rad, sf::Vector2f dir,
+                  float mov_spd,int arg_damage):
+        Projectile(texture_file, position, size, hit_rad, dir, mov_spd,
+                   arg_damage), blast{}
+  {}
   Projectile_bomb(sf::Vector2f position, sf::Vector2f direction);
   ~Projectile_bomb() = default;
 
-  Projectile_bomb(Projectile_bomb const& other); //Kopieringskonstruktor
+  Projectile_bomb(Projectile_bomb const& other); //Copy-constructor
   void clone(sf::Vector2f dir, sf::Vector2f pos);
   bool collision();
+  //static
   static int frames_to_live;
   static int damage_init;
   static entity_properties prop;
+
 protected:
   Projectile_bomb_blast blast;
-  void new_bomb_blast(sf::Vector2f position);
-  // std::string texture_file;
-  // float hit_rad;
-  // float mov_spd;
-
 };
 
 #endif
